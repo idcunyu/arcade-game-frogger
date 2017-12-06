c20179eda8e979848d725d075543b808c5aa771d
sansregret <695035427@qq.com>
2017-12-06 20:01:38 +0800

    1.0.2

--- js/app.js
+++ js/app.js
@@ -1,80 +1,108 @@
 /*
+ * 对于**玩家角色**：
+ * 设定画布中横向每一格101px,竖向每一格83px。
+ * 横向从左至右：0、101、202、303、404。——>x+0
+ * 竖向从上到下：0、83、166、249、332、415。——>y-11
+ *
+ * 对于**敌人**：
+ * 设定画布中横向每一格101px,竖向每一格83px。
+ * 横向从左至右：0、101、202、303、404。——>x+20（使其在改变大小后显示居中）
+ * 竖向从上到下：0、83、166、249、332、415。——>y+45（使其在改变大小后处于应该在的位置，并显示居中）
+ */
+let horNum = [0,101,202,303,404];
+let verNum = [0,83,166,249,332,415];
+let verNumEnemy = [83,166,249,332];
+
+/*
  * 敌人类
  */
-var Enemy = function() {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
+var Enemy = function(x = 0, y = 83 ,speed = 200) {
+  // 要应用到每个敌人的实例的变量写在这里
+  // 我们已经提供了一个来帮助你实现更多
 
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
-    this.sprite = 'images/enemy-bug.png';
+  // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+  this.sprite = 'images/enemy-bug.png';
+
+  // x,y
+  this.x = x;
+  this.y = y;
+
+  this.speed = speed;
+
+  this.going=Math.random()*this.speed;
 };
 
-// 此为游戏必须的函数，用来更新敌人的位置
-// 参数: dt ，表示时间间隙
-Enemy.prototype.update = function(dt) {
-    // console.log("敌人位置移动");
-    // 应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上都是以同样的速度运行的
-};
-
-// 此为游戏必须的函数，用来在屏幕上显示出敌人
+// 在屏幕上显示出敌人
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // console.log("显示出一个敌人");
+  ctx.drawImage(Resources.get(this.sprite), this.x-81, this.y+45,60,102);
+
+};
+
+// 更新敌人的位置（参数: dt ，表示时间间隙）
+Enemy.prototype.update = function(dt) {
+  // 应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上都是以同样的速度运行的
+
+  this.x+=this.going*dt;
+  // console.log(dt);
 };
 
 
 /*
  * 玩家类
  */
-var Player=function(){
+var Player = function() {
   // 玩家的图片或者雪碧图
   this.sprite = 'images/char-boy.png';
 };
 
-// 此为游戏必须的函数，用来更新玩家的位置
-// 参数: dt ，表示时间间隙
+// 在屏幕上显示出玩家角色
+Player.prototype.render = function(x = 202, y = 415) {
+  ctx.drawImage(Resources.get(this.sprite), x, y-11);
+
+};
+
+// 更新玩家的位置（参数: dt ，表示时间间隙）
 Player.prototype.update = function(dt) {
-    // console.log("玩家位置移动");
-    // 应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上都是以同样的速度运行的
+  // 应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上都是以同样的速度运行的
+
 };
 
-// 此为游戏必须的函数，用来在屏幕上显示出玩家角色
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), 202,0);
-    // console.log("显示出玩家！");
-};
-
-// 此为游戏必须的函数，通过键盘的方向键输入来控制玩家的移动前后左右移动
-Player.prototype.handleInput = function(){
+// 通过键盘的方向键输入来控制玩家的移动
+Player.prototype.handleInput = function() {
 
 };
 
 
 /*
- * 现在实例化所需要的所有对象
- * 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
- * 把玩家对象放进一个叫 player 的变量里面
+ * 现在实例化所需要的所有对象。
+ * 把所有敌人的对象都放进一个叫 allEnemies 的数组里面，
+ * 把玩家对象放进一个叫 player 的变量里面。
  */
-var e1=new Enemy();
-var e2=new Enemy();
-var e3=new Enemy();
-var allEnemies=[e1,e2,e3];
-var player=new Player();
-
+// var e1 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e2 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e3 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e4 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e5 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e6 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e7 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var e8 = new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500);
+// var allEnemies = [e1, e2, e3,e4,e5,e6,e7,e8];
+var allEnemies = [];
+allEnemies.push(new Enemy(0, verNumEnemy[Math.floor((Math.random()*verNumEnemy.length))]-20,Math.random()*500));
+var player = new Player();
 
 /*
  * 这段代码监听游戏玩家的键盘点击事件，
  * 并且代表将按键的关键数字送到 Play.handleInput()方法里面。
- *
- * 不需要再更改这段代码了。
+ * 这段代码无需再做更改
  */
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
